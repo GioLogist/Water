57a991ff6133eb3aa1b5c44c061e0d049eea0e14
GioLogist <gio@giologist.com>
2017-05-31 02:13:09 -0700

    Move StyleGuide helper & remove debug info

--- styleguide/custom-builder/helpers/helpers.js
+++ styleguide/custom-builder/helpers/helpers.js
@@ -5,10 +5,7 @@ var fs = require("fs");
 module.exports = function(Twig) {
 
   Twig.extendFunction('file_get_contents', function(path) {
-    console.log(path,'test');
-
     var contents = fs.readFileSync(path, 'utf8');
-    console.log(contents)
     return contents;
   });
 
