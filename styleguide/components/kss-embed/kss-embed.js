81c3e236d00409d9dc86f6b9963ba7058c0656f6
GioLogist <gio@giologist.com>
2017-05-31 02:17:17 -0700

    StyleGuide Embed Widget

---
+++ styleguide/components/kss-embed/kss-embed.js
@@ -0,0 +1,12 @@
+$(function() {
+  // activate tabs
+  $(".kss-embed").tabs({
+    active: 1
+  });
+  // render html inside iframe
+  $(".kss-embed__html").each(function(i) {
+    var iframe = $(this).find('.kss-embed__html-iframe');
+    var html_code = $(this).find('.kss-embed__html-code').html();
+    $(iframe).attr('srcdoc', html_code);
+  });
+});
