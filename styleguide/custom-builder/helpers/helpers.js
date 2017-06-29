f35663d0e50e85125df0ad0764cbaf3d18c0f057
GioLogist <gio@giologist.com>
2017-06-29 16:58:56 -0700

    StyleGuide: support to detect if file exists via twig

--- styleguide/custom-builder/helpers/helpers.js
+++ styleguide/custom-builder/helpers/helpers.js
@@ -5,7 +5,12 @@ var fs = require("fs");
 module.exports = function(Twig) {
 
   Twig.extendFunction('file_get_contents', function(path) {
-    var contents = fs.readFileSync(path, 'utf8');
+
+    if(fs.existsSync(path)) {
+      var contents = fs.readFileSync(path, 'utf8');
+    } else {
+      var contents = false;
+    }
     return contents;
   });
 
